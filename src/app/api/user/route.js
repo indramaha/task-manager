57a3f30fbeb0f0ebd29dad6367c9dcf9a8249c9f
komangsuryasedana <komangsuryasedana@mail.com>
2024-03-06 13:14:34 +0800

    chore: test

--- src/app/api/user/route.js
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../../../app/api/auth/[...nextauth]/route";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const session = await getServerSession(authOptions)
-  return NextResponse.json(session)
-}
--- src/lib/db.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
-
-export const db = prisma
